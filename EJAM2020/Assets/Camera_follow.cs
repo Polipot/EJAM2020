95b4bd927678839609c78c967c67fcf55f7403d9
Bryndye <hugocluzel17@hotmail.fr>
2020-06-27 13:29:05 +0200

    Player déplacement + camera
    la camera nest pas smooth

---
+++ EJAM2020/Assets/Camera_follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_follow : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        SmoothFollow();
+    }
+
+    void SmoothFollow()
+    {
+        if (target != null)
+        {
+            Vector3 smooth = target.position - transform.position;
+            transform.position += smooth / 20;
+        }
+    }
+}
---
+++ EJAM2020/Assets/Player_Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float Speed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+        LookMouse();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * Speed, 0, Input.GetAxis("Vertical") * Speed);
+    }
+
+    void LookMouse()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.y = 0;
+        transform.LookAt(mousePos);
+    }
+}
+
+/*
+ *         Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+ * */
