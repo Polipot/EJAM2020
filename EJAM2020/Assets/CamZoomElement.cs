c434f53dcc943613a803a6af9abcda98aadfedfe
Bryndye <hugocluzel17@hotmail.fr>
2020-06-28 22:38:12 +0200

    afazezrerjty

---
+++ EJAM2020/Assets/CamZoomElement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamZoomElement : MonoBehaviour
+{
+    bool first = false;
+
+    private void Update()
+    {
+        if (IAManager.Instance.PolicemanOnGround && !first)
+        {
+            first = true;
+        }
+    }
+
+    void zoomPolice()
+    {
+       // transform.position = 
+    }
+}
