f5282133cd3e02b5a0751e3786db05e3fbd1dcd6
Jules Rouault <trustprinciples@gmail.com>
2020-06-28 19:58:31 +0200

    Gogo la gogole

---
+++ EJAM2020/Assets/Script/Environnement/CourtCircuit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CourtCircuit : MonoBehaviour
+{
+    [Header("Attributs")]
+    public bool Actif;
+    public float Latence;
+    float Temps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Actif)
+        {
+            Temps += Time.deltaTime;
+            if(Temps >= Latence)
+            {
+                Actif = false;
+            }
+        }
+    }
+
+    public void Activation()
+    {
+        Actif = true;
+    }
+}
