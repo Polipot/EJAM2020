1481091444ae875beabb3d7f135a2670302492c3
Bryndye <hugocluzel17@hotmail.fr>
2020-06-28 02:20:46 +0200

    Tuto.Text
    Lorsqu'on lance la partie, on se fait aggro par un texte qui explique vite fait le jeu. Les textes sur les personnes existent mais pas encore implémentés. J'ai pas trouvé comment le faire encore! <3

--- EJAM2020/Assets/Tuto.cs
+++ EJAM2020/Assets/Tuto.cs
@@ -1,18 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tuto : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool isActive = true;
+
+    int step;
+    public GameObject canvasTuto;
+    public GameObject bt;
+    public GameObject[] Blocs;
+
+    public string inno, garde, police;
+    public GameObject Pepole_g;
+    public Text Pepole;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isActive = !isActive;
+            canvasTuto.SetActive(isActive);
+        }
+
+        switch (step)
+        {
+            case 0:
+                for (int i = 0; i < Blocs.Length; i++)
+                {
+                    Blocs[i].SetActive(false);
+                }
+                Blocs[0].SetActive(true);
+                break;
+
+            case 1:
+                for (int i = 0; i < Blocs.Length; i++)
+                {
+                    Blocs[i].SetActive(false);
+                }
+                Blocs[1].SetActive(true);
+                break;
+
+            case 2:
+                for (int i = 0; i < Blocs.Length; i++)
+                {
+                    Blocs[i].SetActive(false);
+                }
+                Blocs[2].SetActive(true);
+                break;
+
+            case 3:
+                for (int i = 0; i < Blocs.Length; i++)
+                {
+                    Blocs[i].SetActive(false);
+                }
+                bt.SetActive(false);
+                Time.timeScale = 1;
+                break;
+
+            default:
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Pepole_g.SetActive(false);
+
+        if (isActive)
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    public void NextBt()
+    {
+        step++;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<IAMovement>() != null)
+        {
+            IAMovement ia = other.GetComponent<IAMovement>();
+
+            if (ia.myType == Type.Civilian)
+            {
+                Pepole_g.SetActive(true);
+                Pepole.text = inno;
+            }
+
+            if (ia.myType == Type.Guard)
+            {
+                Pepole_g.SetActive(true);
+                Pepole.text = garde;
+            }
+
+            if (ia.myType == Type.Policeman)
+            {
+                Pepole_g.SetActive(true);
+                Pepole.text = police;
+            }
+        }
     }
 }
